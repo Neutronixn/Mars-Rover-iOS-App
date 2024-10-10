605518a3d29ac2209de7af6ef2f9da20cae6f818
nightvisionz <vision@grailbase.com>
2024-10-10 13:42:58 -0500

    Initial Commit

---
+++ Mars Rover Info/Mars_Rover_InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mars_Rover_InfoApp.swift
+//  Mars Rover Info
+//
+//  Created by Vision on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Mars_Rover_InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
